c34af245d7ab45c4981515c369f63f6471cd98d8
= <=>
2024-08-31 15:01:29 -0700

    cin will ignore whitespace and won't error when you enter the incorrect data type.

---
+++ collectingAge.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int age;
+
+	cout << "What is your age?\n";
+	cin >> age;
+	cout << "So, you are " << age << " years old?\n";
+
+	return 0;
+}
