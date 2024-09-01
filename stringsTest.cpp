eeb74db94d874339a24cc90d23d79240a71efe35
Wesley Blake <wesleyblake95@gmail.com>
2024-08-31 20:18:41 -0700

    the <string> library will apparently realocate memory in a string to accomidate different lengths.

--- stringsTest.cpp
+++ stringsTest.cpp
@@ -7,6 +7,11 @@ int main()
 	string test = "Hellow world! This is a library for strings";
 	cout << test << endl;
 
+	string otherTest = "hello";
+	cout << test << endl;
+	otherTest = "Will this give an error like c would?";
+	cout << otherTest << endl;
+
 	return 0;
 }
 
