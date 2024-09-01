90577abfebab8dd69a928bcbaec7424ed7a50d0c
Wesley Blake <wesleyblake95@gmail.com>
2024-08-31 20:31:17 -0700

    I didn't expect that behavior

--- nameTest.cpp
+++ nameTest.cpp
@@ -10,5 +10,15 @@ int main()
 	cin >> name;
 	cout << "Did quotes catch everything? " << name << endl;
 
+	cout << "One more time (will getline catch this?): ";
+	getline (cin, name);
+	cout << "Your name is: " << name << endl;
+	/* output
+	 * What is your name? Wesley Blake
+	 * Did quotes catch everything? Wesley
+	 * One more time (will getline catch this?): Your name is:  Blake
+	*/
+	// I didn't expect that behavior. It didn't prompt me to enter anything, it just took Blake.
+
 	return 0;
 }
