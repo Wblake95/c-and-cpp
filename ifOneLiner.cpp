aaed476b7e8ef3663c78e4137b27458c519bfeda
Wesley Blake <wesleyblake95@gmail.com>
2024-09-01 13:17:16 -0700

    learning one liner

--- ifOneLiner.cpp
+++ ifOneLiner.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
@@ -7,16 +8,21 @@ int main()
 	cout << "What time is it? (do not add : )\n";
 	cin >> time;
 
-	if ( 1200 > time )
+	if ( 1200 >= time )
 	{
 		cout << "Good morning\n";
-	} else if ( 1700 > time )
+	} else if ( 1700 >= time )
 	{
 		cout << "Good afternoon\n";
 	} else
 	{
 		cout << "Good evening\n";
 	}
+
+
+	string result = ( 1200 >= time ) ? "Good afternoon" : "Good morning";
+
+	cout << result << endl;
 
 	return 0;
 
