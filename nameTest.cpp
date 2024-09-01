5fe3ea89190ac8ad1ca6814f227bb1aa5875f9b5
Wesley Blake <wesleyblake95@gmail.com>
2024-08-31 20:25:18 -0700

    Even if you use quotes, it will stop with white space.

---
+++ nameTest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name;
+
+	cout << "What is your name? ";
+	cin >> name;
+	cout << "Did quotes catch everything? " << name << endl;
+
+	return 0;
+}
