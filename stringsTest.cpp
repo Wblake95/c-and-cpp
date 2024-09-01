d0eda014f6bfd7c326a97506f74ea2790eeb73e7
Wesley Blake <wesleyblake95@gmail.com>
2024-08-31 20:05:30 -0700

    learning by typeing from memory rather than copying

---
+++ stringsTest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string test = "Hellow world! This is a library for strings";
+	cout << test << endl;
+
+	return 0;
+}
+
